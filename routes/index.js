b4b336b317f417728e294e5ab22857e4b7bb8c46
skvedula <skvedula@outlook.com>
2017-07-31 18:15:50 +0530

    Adding code for getting PDF label in the req format

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var pdf = require('html-pdf');
+var fs = require('fs');
+var options = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:id', function(req, res, next) {
+	var data = {
+		deliveryid: "delivery.deliveryid",
+		recipientclientid: "delivery.recipientclientid",
+		recipientname: "delivery.recipientname",
+		recipientemail: "delivery@recipientemail" || 'N/A',
+		recipientphone: "delivery.recipientphone",
+		deliveryaddress: "delivery.deliveryaddress"
+	}
+  res.render('layout', {data}, function(err, html){
+		pdf.create(html, options).toStream(function(err, stream){
+			stream.pipe(res);
+		});  	
+	});
+	// res.render('layout', {data});
 });
 
 module.exports = router;
